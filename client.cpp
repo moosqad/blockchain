2784141b5ad5eadd7a6cb5f4530790f776639099
= <smetannikov@smetannikov.local>
2023-03-22 00:38:33 +0300

    cleint-server

---
+++ client.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <boost/asio.hpp>
+
+using boost::asio::ip::tcp;
+
+int main(int argc, char* argv[]) {
+  try {
+    // Get the transaction details from command line arguments
+    if (argc != 4) {
+      std::cerr << "Usage: client <sender> <receiver> <amount>" << std::endl;
+      return 1;
+    }
+
+    std::string sender = argv[1];
+    std::string receiver = argv[2];
+    float amount = std::stof(argv[3]);
+
+    // Create an io_context object
+    boost::asio::io_context io_context;
+
+    // Create a tcp::resolver object to resolve the server address
+    tcp::resolver resolver(io_context);
+    tcp::resolver::results_type endpoints = resolver.resolve("localhost", "8080");
+
+    // Create a tcp::socket object and connect to the server
+    tcp::socket socket(io_context);
+    boost::asio::connect(socket, endpoints);
+
+    // Form the transaction message and send it to the server
+    std::string message = sender + " sent " + std::to_string(amount) + " coins to " + receiver;
+    boost::asio::write(socket, boost::asio::buffer(message));
+
+    // Read the response from the server
+    boost::asio::streambuf response;
+    boost::asio::read_until(socket, response, '\n');
+
+    // Print the response
+    std::string response_data = boost::asio::buffer_cast<const char*>(response.data());
+    std::cout << response_data << std::endl;
+
+    return 0;
+  }
+  catch (std::exception& e) {
+    std::cerr << "Exception: " << e.what() << std::endl;
+    return 1;
+  }
+}
+
---
+++ server.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <boost/asio.hpp>
+#include <thread>
+#include <vector>
+#include "blockchain.cpp"
+
+using boost::asio::ip::tcp;
+
+// Define a function to handle incoming client connections
+void handleConnection(tcp::socket socket, Blockchain& blockchain) {
+  try {
+    std::cout << "New client connected." << std::endl;
+
+    // Define a buffer to hold incoming data
+    char data[1024];
+
+    // Read the incoming data from the socket
+    size_t len = socket.read_some(boost::asio::buffer(data, 1024));
+
+    // Parse the incoming data to get the transaction details
+    std::string txString(data, len);
+    std::istringstream iss(txString);
+    std::string sender, receiver;
+    float amount;
+    iss >> sender >> receiver >> amount;
+
+    // Add the transaction to the blockchain
+    Transaction tx(sender, receiver, amount);
+    Block newBlock(blockchain.chain.size(), {tx}, blockchain.getLastBlock().hash);
+    blockchain.addBlock(newBlock);
+
+    // Send the updated blockchain to all connected clients
+    std::string chainString = blockchain.toString();
+    for (tcp::socket& client : connectedClients) {
+      client.write_some(boost::asio::buffer(chainString));
+    }
+
+    // Close the socket
+    socket.close();
+  } catch (std::exception& e) {
+    std::cerr << "Exception in thread: " << e.what() << std::endl;
+  }
+}
+
+// Define a vector to hold all connected clients
+std::vector<tcp::socket> connectedClients;
+
+int main() {
+  try {
+    // Create a blockchain instance
+    Blockchain blockchain = Blockchain(4);
+
+    // Create an io_context object
+    boost::asio::io_context io_context;
+
+    // Create a tcp::acceptor object to listen for incoming connections
+    tcp::acceptor acceptor(io_context, tcp::endpoint(tcp::v4(), 8080));
+
+    // Start accepting incoming connections
+    while (true) {
+      tcp::socket socket(io_context);
+      acceptor.accept(socket);
+
+      // Add the client socket to the vector of connected clients
+      connectedClients.push_back(std::move(socket));
+
+      // Start a new thread to handle the incoming connection
+      std::thread(handleConnection, std::move(socket), std::ref(blockchain)).detach();
+    }
+  } catch (std::exception& e) {
+    std::cerr << "Exception: " << e.what() << std::endl;
+  }
+
+  return 0;
+}
+
